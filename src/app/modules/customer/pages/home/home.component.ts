962a713221362431bd6435ec554bfa8c25b6a04a
luizgcl <luiizgcl@gmail.com>
2025-01-09 15:47:02 -0300

    feat(customer): adiciona menu + drawer na home do modulo

--- src/app/modules/customer/pages/home/home.component.ts
+++ src/app/modules/customer/pages/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Avatar } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
+import { DrawerModule } from 'primeng/drawer';
+import { IconField } from 'primeng/iconfield';
+import { InputIcon } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { Toolbar } from 'primeng/toolbar';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Toolbar, ButtonModule, DrawerModule, Avatar, InputTextModule, IconField, InputIcon],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  visible = false;
 }
