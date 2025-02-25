5fdf3c3e7a80ff9eeb9fbc6ace84c99d4a41c97b
luizgcl <luiizgcl@gmail.com>
2025-02-25 19:17:36 -0300

    add link to back homepage when user click in logo

--- src/app/components/logo/logo.component.ts
+++ src/app/components/logo/logo.component.ts
@@ -1,13 +1,14 @@
 import { Component, input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 type LogoVariant = 'colored' | 'primary' | 'all-black';
 
 @Component({
   selector: 'app-logo',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './logo.component.html',
-  styleUrl: './logo.component.scss'
+  styleUrl: './logo.component.scss',
 })
 export class LogoComponent {
-  variant = input<LogoVariant>('colored')
+  variant = input<LogoVariant>('colored');
 }
